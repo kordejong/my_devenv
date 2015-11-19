767a685d57af525e1f144fff274219ff8754f260
Kor de Jong <kor@jemig.eu>
2015-11-19 15:48:37 +0100

    Add performance_analyst project config

---
+++ configuration/project/performance_analyst.sh
@@ -0,0 +1,18 @@
+cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+source $cwd/util.sh
+unset cwd
+
+
+parse_commandline $*
+
+
+if [ ! "$PERFORMANCE_ANALYST" ]; then
+    export PERFORMANCE_ANALYST="$PROJECTS/`\ls $PROJECTS | \grep -i \"^performance_analyst$\"`"
+fi
+
+export PATH="$PERFORMANCE_ANALYST/environment/script:$PATH"
+
+
+set_prompt_for_project performance_analyst $MY_DEVENV_BUILD_TYPE
+
+cd $PERFORMANCE_ANALYST
