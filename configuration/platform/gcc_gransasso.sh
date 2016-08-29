7b93b09b5fb5bf14e503a56ccd52a4a28c58a023
Kor de Jong <kor@jemig.eu>
2016-08-29 10:51:54 +0200

    Update gcc config gransasso

--- configuration/platform/gcc_gransasso.sh
+++ configuration/platform/gcc_gransasso.sh
@@ -3,5 +3,7 @@ source $cwd/gransasso.sh
 # export CC=/opt/gcc-4.9/bin/gcc
 # export CXX=/opt/gcc-4.9/bin/g++
 # export LD_LIBRARY_PATH=/opt/gcc-4.9/lib64:$LD_LIBRARY_PATH
-export CC=gcc-4.9
-export CXX=g++-4.9
+# export CC=gcc-4.9
+# export CXX=g++-4.9
+export CC=gcc-5
+export CXX=g++-5
