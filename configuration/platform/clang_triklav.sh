c1821da29fa18084aa140721c508f482fa2e4b37
Kor de Jong <kor@jemig.eu>
2017-10-09 18:24:44 +0200

    Update config

--- configuration/platform/clang_triklav.sh
+++ configuration/platform/clang_triklav.sh
@@ -1,4 +1,4 @@
 cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $cwd/triklav.sh
-export CC=/opt/local/bin/clang-mp-4.0
-export CXX=/opt/local/bin/clang++-mp-4.0
+export CC=/opt/local/bin/clang-mp-5.0
+export CXX=/opt/local/bin/clang++-mp-5.0
