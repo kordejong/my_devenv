e3d9d9157528e8bb952bb956bd9a687dd06b4607
Kor de Jong <kor@jemig.eu>
2017-11-02 18:59:06 +0100

    Update config

--- configuration/project/natural_capital_models.sh
+++ configuration/project/natural_capital_models.sh
@@ -11,19 +11,34 @@ if [ ! "$NATURAL_CAPITAL_MODELS" ]; then
 fi
 
 
-export PATH="$NATURAL_CAPITAL_MODELS/environment/script:$NATURAL_CAPITAL_MODELS/source/script:$PATH"
-export PYTHONPATH="$NATURAL_CAPITAL_MODELS/source/package:$PYTHONPATH"
+PATH="$NATURAL_CAPITAL_MODELS/environment/script:$NATURAL_CAPITAL_MODELS/source/script:$PATH"
+PYTHONPATH="$NATURAL_CAPITAL_MODELS/source/package:$PYTHONPATH"
 
 # gransasso
-project_data=/mnt/data2/kor/project/natural_capital_models
+
+
+if [[ `hostname` == triklav\.* ]]; then
+
+    project_data=$HOME/tmp
+
+    pcraster_prefix=$OBJECTS/Debug/pcraster
+    PATH=$pcraster_prefix/bin:$PATH
+    PYTHONPATH=$pcraster_prefix/bin:$PYTHONPATH
+    unset pcraster_prefix
+elif [[ `hostname` == "gransasso" ]]; then
+    project_data=/mnt/data2/kor/project/natural_capital_models
+fi
 
 export NCM_WORKSPACE=$project_data/data
 export NCM_CONFIGURATION=$NATURAL_CAPITAL_MODELS/environment/configuration/ncm.ini
+export PATH PYTHONPATH
+
 
 unset project_data
 
 
 # mkvirtualenv --python /usr/bin/python2 --system-site-packages natural_capital_models
+# mkvirtualenv --python `which python3.5` --system-site-packages natural_capital_models
 workon natural_capital_models
 
 cd $NATURAL_CAPITAL_MODELS
