fc9107d1614146234aa4a19940cb23b8b0cf1b89
Kor de Jong <kor@jemig.eu>
2017-11-07 17:01:13 +0100

    Start using clang-5

--- configuration/platform/clang_gransasso.sh
+++ configuration/platform/clang_gransasso.sh
@@ -1,4 +1,5 @@
 cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $cwd/gransasso.sh
-export CC="clang-3.7"
-export CXX="clang++-3.7"
+export CC="clang-5.0"
+export CXX="clang++-5.0"
+export CLANG_FORMAT_PY=/usr/share/clang/clang-format-5.0/clang-format.py
--- configuration/platform/gransasso.sh
+++ configuration/platform/gransasso.sh
@@ -1,3 +1,4 @@
 cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $cwd/gnu_make.sh
 export MAKEFLAGS="$MAKEFLAGS -j9"
+export CLANG_FORMAT_PY=/usr/share/clang/clang-format-5.0/clang-format.py
