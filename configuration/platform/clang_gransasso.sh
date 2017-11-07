fe274d706c930f70fdcc31336961cfb6fc18cfe9
Kor de Jong <kor@jemig.eu>
2017-11-07 17:03:04 +0100

    Lingering line

--- configuration/platform/clang_gransasso.sh
+++ configuration/platform/clang_gransasso.sh
@@ -2,4 +2,3 @@ cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $cwd/gransasso.sh
 export CC="clang-5.0"
 export CXX="clang++-5.0"
-export CLANG_FORMAT_PY=/usr/share/clang/clang-format-5.0/clang-format.py
