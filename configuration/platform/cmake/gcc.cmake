bcfc97ace8140a9016aedb9c91ea9e6e6721b828
Kor de Jong <kor@jemig.eu>
2019-01-07 12:02:40 +0100

    Add sanitize flags

--- configuration/platform/cmake/gcc.cmake
+++ configuration/platform/cmake/gcc.cmake
@@ -14,3 +14,22 @@ string(REPLACE "\n" "" c_flags ${c_flags})
 string(REPLACE "\n" "" cxx_flags ${cxx_flags})
 set(CMAKE_C_FLAGS_INIT ${c_flags})
 set(CMAKE_CXX_FLAGS_INIT ${cxx_flags})
+
+
+# Debug flags
+set(shared_debug_flags "
+    -fsanitize=address
+    -fsanitize=leak
+    -fsanitize=undefined
+    -fno-omit-frame-pointer
+")
+set(c_debug_flags "
+    ${shared_debug_flags}
+")
+set(cxx_debug_flags "
+    ${shared_debug_flags}
+")
+string(REPLACE "\n" "" c_debug_flags ${c_debug_flags})
+string(REPLACE "\n" "" cxx_debug_flags ${cxx_debug_flags})
+set(CMAKE_C_FLAGS_DEBUG_INIT ${c_debug_flags})
+set(CMAKE_CXX_FLAGS_DEBUG_INIT ${cxx_debug_flags})
