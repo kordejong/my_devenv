965d2fd6216969b33bd50f1de2a322928c261813
Kor de Jong <kor@jemig.eu>
2019-01-07 12:48:03 +0100

    Comment sanitize options

--- configuration/platform/cmake/gcc.cmake
+++ configuration/platform/cmake/gcc.cmake
@@ -31,5 +31,6 @@ set(cxx_debug_flags "
 ")
 string(REPLACE "\n" "" c_debug_flags ${c_debug_flags})
 string(REPLACE "\n" "" cxx_debug_flags ${cxx_debug_flags})
-set(CMAKE_C_FLAGS_DEBUG_INIT ${c_debug_flags})
-set(CMAKE_CXX_FLAGS_DEBUG_INIT ${cxx_debug_flags})
+# TODO Find a way to only see messages about our own code
+# set(CMAKE_C_FLAGS_DEBUG_INIT ${c_debug_flags})
+# set(CMAKE_CXX_FLAGS_DEBUG_INIT ${cxx_debug_flags})
