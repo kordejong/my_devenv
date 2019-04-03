ea12bec56ca1a2caabeb085d1355d02c599b8e1b
Kor de Jong <kor@jemig.eu>
2019-04-03 17:15:51 +0200

    Ported to new clang

--- configuration/platform/cmake/triklav.cmake
+++ configuration/platform/cmake/triklav.cmake
@@ -4,7 +4,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/darwin.cmake)
 
 # Tools
 # Compiler
-set(CMAKE_C_COMPILER /opt/local/bin/clang-mp-6.0)
-set(CMAKE_CXX_COMPILER /opt/local/bin/clang++-mp-6.0)
+set(CMAKE_C_COMPILER /opt/local/bin/clang-mp-8.0)
+set(CMAKE_CXX_COMPILER /opt/local/bin/clang++-mp-8.0)
 
 include(${CMAKE_CURRENT_LIST_DIR}/clang.cmake)
