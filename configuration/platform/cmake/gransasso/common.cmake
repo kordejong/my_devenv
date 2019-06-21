5691940699ffb2b53f41e5945cecea65fb0c2c29
Kor de Jong <kor@jemig.eu>
2019-06-21 17:05:18 +0200

    Update gransasso toolchain files

--- configuration/platform/cmake/gransasso/common.cmake
+++ configuration/platform/cmake/gransasso/common.cmake
@@ -10,6 +10,11 @@ set(CMAKE_CXX_COMPILER g++-7)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
 
+set(PAPI_ROOT $ENV{HOME}/development/ext CACHE PATHNAME "")
+set(OTF2_ROOT $ENV{HOME}/development/ext/x86_64/scorep-gcc CACHE PATHNAME "")
+# set(OTF2_LIBRARY $ENV{HOME}/development/ext CACHE PATHNAME "")
+# set(OTF2_INCLUDE_DIR
+
 
 # HPX library
 include(${CMAKE_CURRENT_LIST_DIR}/../hpx.cmake)
