3d2a5ec3e91127e8d19f4ba765467f06008fee59
Kor de Jong <k.dejong1@uu.nl>
2020-01-16 12:08:34 +0100

    Update HPX config

--- configuration/platform/cmake/login01/hpx.cmake
+++ configuration/platform/cmake/login01/hpx.cmake
@@ -6,12 +6,6 @@ set(HPX_WITH_HWLOC ON CACHE BOOL "")
 
 set(HPX_WITH_GOOGLE_PERFTOOLS ON CACHE BOOL "")
 
-# If you are building HPX for a system with more
-# than 64 processing units you must change the CMake
-# variables HPX_WITH_MORE_THAN_64_THREADS (to On) and
-# HPX_WITH_MAX_CPU_COUNT (to a value at least as big as the
-# number of (virtual) cores on your system).
-set(HPX_WITH_MORE_THAN_64_THREADS ON CACHE BOOL "")
 set(HPX_WITH_MAX_CPU_COUNT "96" CACHE STRING "")
 
 set(HPX_WITH_PARCELPORT_TCP ON CACHE BOOL "")
