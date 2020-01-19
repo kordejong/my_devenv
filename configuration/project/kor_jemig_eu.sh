58edbce318c76d0ef12254d2e6e37d491a1304e9
Kor de Jong <kor@jemig.eu>
2020-01-19 15:47:10 +0100

    Update config kor_jemig_eu

--- configuration/project/kor_jemig_eu.sh
+++ configuration/project/kor_jemig_eu.sh
@@ -14,17 +14,29 @@ KOR_JEMIG_EU_CMAKE_ARGUMENTS="
     -DCMAKE_INSTALL_PREFIX:PATH=${TMPDIR:-/tmp}/$MY_DEVENV_BUILD_TYPE/$basename
 "
 
-cmake_toolchain_file=$EMSDK/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake
+### cmake_toolchain_file=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+### 
+### if [ ! -f $cmake_toolchain_file ]; then
+###     echo "INFO: No CMake toolchain file found for a $MY_DEVENV_BUILD_TYPE build on $hostname"
+###     echo "INFO: Tried $cmake_toolchain_file"
+###     echo "INFO: Did you 'source ~/opt/emsdk/emsdk_env.sh --build Release'?"
+###     echo "INFO: No worries, you just won't be able to use emscripten now"
+### else
+###     KOR_JEMIG_EU_CMAKE_ARGUMENTS="
+###         $KOR_JEMIG_EU_CMAKE_ARGUMENTS
+###         -DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain_file
+###     "
+### fi
 
-if [ ! -f $cmake_toolchain_file ]; then
-    echo "INFO: No CMake toolchain file found for a $MY_DEVENV_BUILD_TYPE build on $hostname"
-    echo "INFO: Tried $cmake_toolchain_file"
-else
-    KOR_JEMIG_EU_CMAKE_ARGUMENTS="
-        $KOR_JEMIG_EU_CMAKE_ARGUMENTS
-        -DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain_file
-    "
-fi
+
+### repository_cache="$HOME/development/repository"
+### if [ -d "$repository_cache" ]; then
+###     KOR_JEMIG_EU_CMAKE_ARGUMENTS="
+###         $KOR_JEMIG_EU_CMAKE_ARGUMENTS
+###         -DNOC_REPOSITORY_CACHE:PATH=$repository_cache
+###     "
+### fi
+
 
 export KOR_JEMIG_EU
 export KOR_JEMIG_EU_CMAKE_ARGUMENTS
