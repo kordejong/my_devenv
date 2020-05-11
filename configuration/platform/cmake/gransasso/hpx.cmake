e92c08de7872a0b85e86aba61bb9b371f39b59cc
Kor de Jong <kor@jemig.eu>
2020-05-11 14:44:10 +0200

    Use system allocator for now

--- configuration/platform/cmake/gransasso/hpx.cmake
+++ configuration/platform/cmake/gransasso/hpx.cmake
@@ -1,7 +1,8 @@
 # Machine-dependent configuration options that don't depend on the
 # build configuration
 
-set(HPX_WITH_MALLOC "tcmalloc" CACHE STRING "")
+# set(HPX_WITH_MALLOC "tcmalloc" CACHE STRING "")
+set(HPX_WITH_MALLOC "system" CACHE STRING "")
 set(HPX_WITH_HWLOC ON CACHE BOOL "")
 
 set(HPX_WITH_GOOGLE_PERFTOOLS ON CACHE BOOL "")
