efd73201e538c12b213e1274a4dc8082c0125215
Kor de Jong <kor@jemig.eu>
2020-07-16 10:55:11 +0200

    Use tcmalloc again

--- configuration/platform/cmake/gransasso/hpx.cmake
+++ configuration/platform/cmake/gransasso/hpx.cmake
@@ -1,8 +1,7 @@
 # Machine-dependent configuration options that don't depend on the
 # build configuration
 
-# set(HPX_WITH_MALLOC "tcmalloc" CACHE STRING "")
-set(HPX_WITH_MALLOC "system" CACHE STRING "")
+set(HPX_WITH_MALLOC "tcmalloc" CACHE STRING "")
 set(HPX_WITH_HWLOC ON CACHE BOOL "")
 
 set(HPX_WITH_GOOGLE_PERFTOOLS ON CACHE BOOL "")
