232ba77418a0b159073bd61b31ff7c87443c1050
Kor de Jong <k.dejong1@uu.nl>
2020-08-19 13:04:59 +0200

    Update HPX config

--- configuration/platform/cmake/login01/common.cmake
+++ configuration/platform/cmake/login01/common.cmake
@@ -11,10 +11,11 @@ set(CMAKE_CXX_COMPILER g++)
 include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
 
 # The environment variables are set in the script loading the modules
-set(BOOST_ROOT $ENV{BOOST_ROOT} CACHE PATH "")
 set(GLEW_ROOT $ENV{GLEW_ROOT} CACHE PATH "")
 set(HWLOC_ROOT $ENV{HWLOC_ROOT} CACHE PATH "")
 set(PAPI_ROOT $ENV{PAPI_ROOT} CACHE PATH "")
+
+set(PYBIND11_PYTHON_VERSION "3" CACHE STRING "")
 
 # HPX library
 include(${CMAKE_CURRENT_LIST_DIR}/../hpx.cmake)
