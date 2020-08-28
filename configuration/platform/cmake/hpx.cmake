1a3ba35b85a16929cee15f3d38a0b6b4735d76db
Kor de Jong <kor@jemig.eu>
2020-08-28 16:26:04 +0200

    Update HPX configuration

--- configuration/platform/cmake/hpx.cmake
+++ configuration/platform/cmake/hpx.cmake
@@ -1,6 +1,9 @@
 # Default configuration settings that don't depend on the machine
 # and build configuration
 
+# When overriding these settings in other CMake scripts, do not forget to
+# pass the FORCE argument. Otherwise the new setting won't have an effect.
+
 set(HPX_WITH_TOOLS ON CACHE BOOL "")
 
 set(HPX_WITH_TESTS OFF CACHE BOOL "")
