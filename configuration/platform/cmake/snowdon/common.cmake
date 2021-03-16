b8116c8091cdcfc5b5004ecf77f6f95b2d95ba93
Kor de Jong <kor@jemig.eu>
2021-03-16 15:55:39 +0100

    Update platform configuration for snowdon

--- configuration/platform/cmake/snowdon/common.cmake
+++ configuration/platform/cmake/snowdon/common.cmake
@@ -5,12 +5,12 @@ include(${CMAKE_CURRENT_LIST_DIR}/../linux.cmake)
 
 # Tools
 # Compiler
-set(CMAKE_C_COMPILER gcc-8)
-set(CMAKE_CXX_COMPILER g++-8)
+set(CMAKE_C_COMPILER gcc-9)
+set(CMAKE_CXX_COMPILER g++-9)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
 
-set(PYBIND11_PYTHON_VERSION "2.7" CACHE STRING "")
+set(PYBIND11_PYTHON_VERSION "3" CACHE STRING "")
 
 # HPX library
 include(${CMAKE_CURRENT_LIST_DIR}/../hpx.cmake)
--- configuration/platform/gnu_make.sh
+++ configuration/platform/gnu_make.sh
@@ -1,2 +1,2 @@
-export MAKEFLAGS="$MAKEFLAGS --quiet"
-export CTEST_OUTPUT_ON_FAILURE=1
+# export MAKEFLAGS="$MAKEFLAGS --quiet"
+# export CTEST_OUTPUT_ON_FAILURE=1
