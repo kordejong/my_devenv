4a10d3c2acd3730c932b04526759a1cdba6c9cfe
Kor de Jong <k.dejong1@uu.nl>
2021-03-31 10:50:37 +0200

    Handle rpath, prefer parallel hdf5, tcp networking

--- configuration/platform/cmake/login01/common.cmake
+++ configuration/platform/cmake/login01/common.cmake
@@ -8,6 +8,24 @@ include(${CMAKE_CURRENT_LIST_DIR}/../linux.cmake)
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 
+# For some reason the paths to these libs don't end up in the RPATH
+# of exes. Possibly because these packages have not been installed
+# properly.
+set(rpaths
+    $ENV{ZSTD_ROOT}/lib
+    $ENV{GCC_DIR}/lib64
+    $ENV{BOOST_ROOT}/stage/lib
+    $ENV{HWLOC_ROOT}/lib
+    $ENV{GOOGLE_PERFTOOLS_ROOT}/lib
+    $ENV{PAPI_ROOT}/libpfm4/lib
+    $ENV{PAPI_ROOT}/src
+)
+set(CMAKE_BUILD_RPATH ${rpaths} CACHE STRING "")
+set(CMAKE_INSTALL_RPATH ${rpaths} CACHE STRING "")
+
+# Use same RPATH at install and build
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE CACHE BOOL "")
+
 include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
 
 # The environment variables are set in the script loading the modules
@@ -16,6 +34,8 @@ set(HWLOC_ROOT $ENV{HWLOC_ROOT} CACHE PATH "")
 set(PAPI_ROOT $ENV{PAPI_ROOT} CACHE PATH "")
 
 set(PYBIND11_PYTHON_VERSION "3" CACHE STRING "")
+
+set(HDF5_PREFER_PARALLEL TRUE CACHE BOOL "")
 
 # HPX library
 include(${CMAKE_CURRENT_LIST_DIR}/../hpx.cmake)
