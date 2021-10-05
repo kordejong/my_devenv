1f833d316d15c243a5b0ab24851931a591f7bbf5
Kor de Jong <kor@jemig.eu>
2021-10-05 19:41:55 +0200

    Add / update project configs

--- configuration/project/natural_capital_models.sh
+++ configuration/project/natural_capital_models.sh
@@ -11,36 +11,37 @@ if [ ! "$NATURAL_CAPITAL_MODELS" ]; then
 fi
 
 
-PATH="$NATURAL_CAPITAL_MODELS/environment/script:$NATURAL_CAPITAL_MODELS/source/script:$PATH"
+# PATH="$NATURAL_CAPITAL_MODELS/environment/script:$NATURAL_CAPITAL_MODELS/source/script:$PATH"
+PATH="$NATURAL_CAPITAL_MODELS/source/script:$PATH"
 # PYTHONPATH="$NATURAL_CAPITAL_MODELS/source/package:$PYTHONPATH"
 
 # gransasso
 
 
-if [[ `hostname` == triklav\.* ]]; then
-    project_data=$HOME/tmp
-    pcraster_prefix=$OBJECTS/Release/pcraster
-    PCRASTER_NR_WORKER_THREADS=2
-elif [[ `hostname` == "gransasso" ]]; then
-    project_data=/mnt/data2/kor/project/natural_capital_models
-    # pcraster_prefix=/opt/pcraster/bin
-    pcraster_prefix=/opt/pcraster-4.2.0-20180112
-    # pcraster_prefix=/opt/pcraster-4.2
-    PCRASTER_NR_WORKER_THREADS=6
-fi
+# if [[ `hostname` == triklav\.* ]]; then
+#     project_data=$HOME/tmp
+#     pcraster_prefix=$OBJECTS/Release/pcraster
+#     PCRASTER_NR_WORKER_THREADS=2
+# elif [[ `hostname` == "gransasso" ]]; then
+#     project_data=/mnt/data2/kor/project/natural_capital_models
+#     # pcraster_prefix=/opt/pcraster/bin
+#     pcraster_prefix=/opt/pcraster-4.2.0-20180112
+#     # pcraster_prefix=/opt/pcraster-4.2
+#     PCRASTER_NR_WORKER_THREADS=6
+# fi
 
-PATH=$pcraster_prefix/bin:$PATH
-LD_LIBRARY_PATH=$PEACOCK_PREFIX/pcraster/linux/linux/gcc-5/x86_64/lib:$LD_LIBRARY_PATH
-PYTHONPATH=$pcraster_prefix/python:$PYTHONPATH
-unset pcraster_prefix
-
-
-export NCM_WORKSPACE=$project_data/data
-export NCM_CONFIGURATION=$NATURAL_CAPITAL_MODELS/environment/configuration/ncm.ini
-export LD_LIBRARY_PATH PATH PYTHONPATH PCRASTER_NR_WORKER_THREADS
+# PATH=$pcraster_prefix/bin:$PATH
+# LD_LIBRARY_PATH=$PEACOCK_PREFIX/pcraster/linux/linux/gcc-5/x86_64/lib:$LD_LIBRARY_PATH
+# PYTHONPATH=$pcraster_prefix/python:$PYTHONPATH
+# unset pcraster_prefix
 
 
-unset project_data
+# export NCM_WORKSPACE=$project_data/data
+# export NCM_CONFIGURATION=$NATURAL_CAPITAL_MODELS/environment/configuration/ncm.ini
+# export LD_LIBRARY_PATH PATH PYTHONPATH PCRASTER_NR_WORKER_THREADS
+
+
+# unset project_data
 
 
 # mkvirtualenv --python `which python2` natural_capital_models
---
+++ configuration/project/uu.sh
@@ -0,0 +1,31 @@
+cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+source $cwd/util.sh
+unset cwd
+
+parse_commandline $*
+
+
+if [ ! "$UU" ]; then
+    export UU="$PROJECTS/gauja/uu/uu"
+fi
+
+
+basename=`basename $UU`
+
+UU_OBJECTS="$OBJECTS/$MY_DEVENV_BUILD_TYPE/$basename"
+
+unset basename
+
+export UU_OBJECTS
+
+cd $UU
+
+unalias phd 2>/dev/null
+
+hostname=`hostname -s`
+
+conda activate uu
+
+unset hostname
+
+pwd
