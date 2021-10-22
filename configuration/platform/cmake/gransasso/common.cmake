96744c41e13172e4382dd88483b2c2b21e1655fa
Kor de Jong <kor@jemig.eu>
2021-10-22 16:26:01 +0200

    Update configurations

--- configuration/platform/cmake/gransasso/common.cmake
+++ configuration/platform/cmake/gransasso/common.cmake
@@ -10,11 +10,14 @@ set(CMAKE_CXX_COMPILER g++-9)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
 
-set(PAPI_ROOT $ENV{HOME}/development/ext CACHE STRING "")
-set(OTF2_ROOT $ENV{HOME}/development/ext/x86_64/scorep-gcc CACHE STRING "")
+# set(PAPI_ROOT $ENV{HOME}/development/ext CACHE STRING "")
+# set(OTF2_ROOT $ENV{HOME}/development/ext/x86_64/scorep-gcc CACHE STRING "")
 # set(OTF2_LIBRARY $ENV{HOME}/development/ext CACHE PATHNAME "")
 # set(OTF2_INCLUDE_DIR
 
 # HPX library
 include(${CMAKE_CURRENT_LIST_DIR}/../hpx.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/hpx.cmake)
+
+# set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE
+#     "$ENV{HOME}/opt/bin/include_what_you_use" -Xiwyu;any -Xiwyu iwyu -Xiwyu args")
