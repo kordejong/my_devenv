f546ac559cf384b04eac5ea5f2a6f0aee1caa767
Kor de Jong <kor@jemig.eu>
2021-12-16 12:14:36 +0100

    Add project config

---
+++ configuration/project/parallel_computing_course.sh
@@ -0,0 +1,31 @@
+cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+source $cwd/util.sh
+unset cwd
+
+parse_commandline $*
+
+
+if [ ! "$PARALLEL_COMPUTING_COURSE" ]; then
+    export PARALLEL_COMPUTING_COURSE="$PROJECTS/computational_geography/parallel_computing_course"
+fi
+
+
+basename=`basename $PARALLEL_COMPUTING_COURSE`
+
+PARALLEL_COMPUTING_COURSE_OBJECTS="$OBJECTS/$MY_DEVENV_BUILD_TYPE/$basename"
+
+unset basename
+
+export PARALLEL_COMPUTING_COURSE_OBJECTS
+
+cd $PARALLEL_COMPUTING_COURSE
+
+unalias parallel_computing_course 2>/dev/null
+
+hostname=`hostname -s`
+
+conda activate parallel_computing_course
+
+unset hostname
+
+pwd
