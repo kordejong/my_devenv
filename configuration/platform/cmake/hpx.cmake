bc47e0b6c441e2901b1325fb5516a9e3e3e94e1d
Kor de Jong <kor@jemig.eu>
2022-04-14 08:25:46 +0200

    Update LUE cmake and project config

--- configuration/platform/cmake/hpx.cmake
+++ configuration/platform/cmake/hpx.cmake
@@ -4,8 +4,6 @@
 # When overriding these settings in other CMake scripts, do not forget to
 # pass the FORCE argument. Otherwise the new setting won't have an effect.
 
-set(HPX_WITH_TOOLS ON CACHE BOOL "")
-
 set(HPX_WITH_TESTS OFF CACHE BOOL "")
 set(HPX_WITH_EXAMPLES OFF CACHE BOOL "")
 
--- configuration/platform/cmake/snowdon/hpx.cmake
+++ configuration/platform/cmake/snowdon/hpx.cmake
@@ -9,8 +9,3 @@ set(HPX_WITH_GOOGLE_PERFTOOLS ON CACHE BOOL "")
 set(HPX_WITH_NETWORKING ON CACHE BOOL "")
 set(HPX_WITH_PARCELPORT_TCP ON CACHE BOOL "")
 set(HPX_WITH_PARCELPORT_MPI OFF CACHE BOOL "")
-
-# Conan Boost and HPX don't seem to play well. Error message during CMake configuration:
-#     HPX inspect tool requires Boost.Regex
-# For now, don't build the HPX tool.
-set(HPX_WITH_TOOLS OFF CACHE BOOL "")
