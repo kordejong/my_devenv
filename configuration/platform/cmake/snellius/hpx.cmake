4545bd855119eeca9eb3a36578dddf5398c37d23
Kor de Jong <k.dejong1@uu.nl>
2022-06-28 08:54:19 +0200

    Port to snellius

---
+++ configuration/platform/cmake/snellius/hpx.cmake
@@ -0,0 +1,13 @@
+# Machine-dependent configuration options that don't depend on the
+# build configuration
+
+set(HPX_WITH_MALLOC "tcmalloc" CACHE STRING "")
+set(HPX_WITH_HWLOC ON CACHE BOOL "")
+
+set(HPX_WITH_GOOGLE_PERFTOOLS ON CACHE BOOL "")
+
+set(HPX_WITH_MAX_CPU_COUNT "128" CACHE STRING "")
+
+set(HPX_WITH_NETWORKING ON CACHE BOOL "")
+set(HPX_WITH_PARCELPORT_TCP OFF CACHE BOOL "")
+set(HPX_WITH_PARCELPORT_MPI ON CACHE BOOL "")
