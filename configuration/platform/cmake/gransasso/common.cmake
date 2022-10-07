295459044789c44428d87167aadf4a5dd2d97269
Kor de Jong <kor@jemig.eu>
2022-10-07 16:54:47 +0200

    Use mold linker

--- configuration/platform/cmake/gransasso/common.cmake
+++ configuration/platform/cmake/gransasso/common.cmake
@@ -8,22 +8,20 @@ include(${CMAKE_CURRENT_LIST_DIR}/../linux.cmake)
 set(CMAKE_C_COMPILER gcc-11)
 set(CMAKE_CXX_COMPILER g++-11)
 include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
-
-# TODO
-# set(CMAKE_EXE_LINKER_FLAGS_INIT "-B/usr/bin/mold")  # gcc <12.1.0
-# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-B/usr/bin/mold")  # gcc <12.1.0
-# set(CMAKE_STATIC_LINKER_FLAGS_INIT "-B/usr/bin/mold")  # gcc <12.1.0
-
-# set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=mold")  # clang, gcc >=12.1.0
-
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-B/usr/bin/mold")  # gcc <12.1.0
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-B/usr/bin/mold")  # gcc <12.1.0
 
 # set(CMAKE_C_COMPILER gcc-12)
 # set(CMAKE_CXX_COMPILER g++-12)
 # include(${CMAKE_CURRENT_LIST_DIR}/../gcc.cmake)
+# set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=mold")  # gcc >=12.1.0
+# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")  # gcc >=12.1.0
 
 # set(CMAKE_C_COMPILER clang-12)
 # set(CMAKE_CXX_COMPILER clang++-12)
 # include(${CMAKE_CURRENT_LIST_DIR}/../clang.cmake)
+# set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=mold")  # clang
+# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")  # clang
 
 
 # set(PAPI_ROOT $ENV{HOME}/development/ext CACHE STRING "")
