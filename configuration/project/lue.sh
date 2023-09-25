82d9ed7b6e31fd81d6b6a5f6c1f8736c05a0c806
Kor de Jong <kor@jemig.eu>
2023-09-25 13:01:37 +0200

    bootstrap_lue.sh and associated refactorings

--- configuration/project/lue.sh
+++ configuration/project/lue.sh
@@ -51,96 +51,88 @@ then
 fi
 
 #   -DCMAKE_RULE_MESSAGES=OFF
-LUE_CMAKE_ARGUMENTS="
-    -DLUE_DATA_MODEL_WITH_PYTHON_API:BOOL=TRUE
-    -DLUE_DATA_MODEL_WITH_UTILITIES:BOOL=TRUE
-    -DLUE_FRAMEWORK_WITH_PYTHON_API:BOOL=TRUE
-    -DLUE_BUILD_VIEW:BOOL=TRUE
-    -DLUE_BUILD_QA:BOOL=TRUE
-    -DLUE_QA_WITH_PYTHON_API:BOOL=TRUE
-    -DLUE_QA_WITH_TESTS:BOOL=TRUE
-    -DLUE_BUILD_DOCUMENTATION:BOOL=TRUE
-    -DLUE_BUILD_HPX:BOOL=TRUE
-    -DLUE_HPX_REPOSITORY=\"file://$PROJECTS/github/kordejong/hpx\"
-"
+# LUE_CMAKE_ARGUMENTS=""
+# LUE_CMAKE_ARGUMENTS="
+#     -DLUE_HPX_REPOSITORY=\"file://$PROJECTS/github/kordejong/hpx\"
+# "
 # This tag corresponds with HPX 1.8.0
 # -DLUE_HPX_GIT_TAG=v1.9.0-rc1
 # -DLUE_HPX_GIT_TAG=198cba516ea
 
 unset basename
 
-repository_cache="$HOME/development/repository"
-if [ -d "$repository_cache" ]; then
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_REPOSITORY_CACHE:PATH=$repository_cache
-    "
-fi
+### repository_cache="$HOME/development/repository"
+### if [ -d "$repository_cache" ]; then
+###     LUE_CMAKE_ARGUMENTS="
+###         $LUE_CMAKE_ARGUMENTS
+###         -DLUE_REPOSITORY_CACHE:PATH=$repository_cache
+###     "
+### fi
 
-cmake_toolchain_file=$MY_DEVENV/configuration/platform/cmake/$LUE_HOSTNAME/$MY_DEVENV_BUILD_TYPE.cmake
-
-if [ ! -f $cmake_toolchain_file ]; then
-    cmake_toolchain_file=$MY_DEVENV/configuration/platform/cmake/$LUE_HOSTNAME.cmake
-fi
-
-if [ ! -f $cmake_toolchain_file ]; then
-    echo "INFO: No CMake toolchain file found for a $MY_DEVENV_BUILD_TYPE build on $LUE_HOSTNAME"
-else
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain_file
-    "
-fi
+### cmake_toolchain_file=$MY_DEVENV/configuration/platform/cmake/$LUE_HOSTNAME/$MY_DEVENV_BUILD_TYPE.cmake
+### 
+### if [ ! -f $cmake_toolchain_file ]; then
+###     cmake_toolchain_file=$MY_DEVENV/configuration/platform/cmake/$LUE_HOSTNAME.cmake
+### fi
+### 
+### if [ ! -f $cmake_toolchain_file ]; then
+###     echo "INFO: No CMake toolchain file found for a $MY_DEVENV_BUILD_TYPE build on $LUE_HOSTNAME"
+### else
+###     LUE_CMAKE_ARGUMENTS="
+###         $LUE_CMAKE_ARGUMENTS
+###         -DCMAKE_TOOLCHAIN_FILE=$cmake_toolchain_file
+###     "
+### fi
 
 
 if [[ $LUE_HOSTNAME == "gransasso" ]];
 then
-    # Platform for testing use of the Ubuntu packages of 3rd party
-    # software LUE depends on. No LUE_HAVE_<NAME> variables set to FALSE.
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=2
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=5
+    ### # Platform for testing use of the Ubuntu packages of 3rd party
+    ### # software LUE depends on. No LUE_HAVE_<NAME> variables set to FALSE.
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=2
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=5
     # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
-    LUE_ROUTING_DATA="/mnt/data1/home/kor/data/project/routing"
-    LUE_BENCHMARK_DATA="/mnt/data1/home/kor/data/project/lue/benchmark/gransasso/"
+    ### LUE_ROUTING_DATA="/mnt/data1/home/kor/data/project/routing"
+    ### LUE_BENCHMARK_DATA="/mnt/data1/home/kor/data/project/lue/benchmark/gransasso/"
 
     source $LUE/env/bin/activate
 
 elif [[ $LUE_HOSTNAME == "fg-vm12" ]];
 then
-    if [ -d "$repository_cache" ]; then
-        LUE_CMAKE_ARGUMENTS="
-            $LUE_CMAKE_ARGUMENTS
-            -DLUE_REPOSITORY_CACHE:PATH=$(cygpath --mixed $repository_cache)
-        "
-    fi
+    ### if [ -d "$repository_cache" ]; then
+    ###     LUE_CMAKE_ARGUMENTS="
+    ###         $LUE_CMAKE_ARGUMENTS
+    ###         -DLUE_REPOSITORY_CACHE:PATH=$(cygpath --mixed $repository_cache)
+    ###     "
+    ### fi
 
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DCMAKE_TOOLCHAIN_FILE=$(cygpath --mixed $cmake_toolchain_file)
-    "
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DCMAKE_TOOLCHAIN_FILE=$(cygpath --mixed $cmake_toolchain_file)
+    ### "
 
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=1
-        -DLUE_HAVE_BOOST:BOOL=TRUE
-        -DLUE_HAVE_DOCOPT:BOOL=FALSE
-        -DLUE_HAVE_FMT:BOOL=FALSE
-        -DLUE_HAVE_GDAL:BOOL=TRUE
-        -DLUE_HAVE_GLFW:BOOL=FALSE
-        -DLUE_HAVE_HDF5:BOOL=TRUE
-        -DLUE_HAVE_NLOHMANN_JSON:BOOL=FALSE
-        -DLUE_HAVE_PYBIND11:BOOL=FALSE
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=2
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=1
+    ###     -DLUE_HAVE_BOOST:BOOL=TRUE
+    ###     -DLUE_HAVE_DOCOPT:BOOL=FALSE
+    ###     -DLUE_HAVE_FMT:BOOL=FALSE
+    ###     -DLUE_HAVE_GDAL:BOOL=TRUE
+    ###     -DLUE_HAVE_GLFW:BOOL=FALSE
+    ###     -DLUE_HAVE_HDF5:BOOL=TRUE
+    ###     -DLUE_HAVE_NLOHMANN_JSON:BOOL=FALSE
+    ###     -DLUE_HAVE_PYBIND11:BOOL=FALSE
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=2
     # PATH=$LUE_OBJECTS/bin/$MY_DEVENV_BUILD_TYPE:$PATH
     # PYTHONPATH=$LUE_OBJECTS/lib/$MY_DEVENV_BUILD_TYPE:$PYTHONPATH
-    LUE_ROUTING_DATA="$HOME/not_needed_on_windows_yet"
+    ### LUE_ROUTING_DATA="$HOME/not_needed_on_windows_yet"
 
     ### # export PATH="`cygpath --mixed /C/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64`:$PATH"
     ### export CC=C:/PROGRA~2/MICROS~1/2019/COMMUN~1/VC/Tools/MSVC/1429~1.301/bin/Hostx64/x64/cl
@@ -195,59 +187,59 @@ then
 elif [[ $LUE_HOSTNAME == "m1compiler" ]];
 then
     # macOS platform for testing build of LUE using Conda packages. These should be installed.
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=2
-        -DLUE_QA_TEST_HPX_RUNWRAPPER=none
-        -DLUE_QA_TEST_HPX_PARCELPORT=tcp
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=4
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=2
+    ###     -DLUE_QA_TEST_HPX_RUNWRAPPER=none
+    ###     -DLUE_QA_TEST_HPX_PARCELPORT=tcp
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=4
 
     source $LUE/env/bin/activate
 
 elif [[ $LUE_HOSTNAME == "login01" ]];
 then
     # Platform for production runs.
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_BUILD_VIEW:BOOL=FALSE
-        -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
-        -DLUE_HAVE_DOCOPT:BOOL=FALSE
-        -DLUE_HAVE_FMT:BOOL=FALSE
-        -DLUE_HAVE_NLOHMANN_JSON:BOOL=FALSE
-        -DLUE_HAVE_PYBIND11:BOOL=FALSE
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=3
-        -DLUE_QA_TEST_HPX_RUNWRAPPER=mpi
-        -DLUE_QA_TEST_HPX_PARCELPORT=mpi
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=10
-    # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
-    LUE_ROUTING_DATA="/scratch/depfg/jong0137/data/routing"
-    LUE_BENCHMARK_DATA="/scratch/depfg/jong0137/data/project/lue/benchmark"
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_BUILD_VIEW:BOOL=FALSE
+    ###     -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
+    ###     -DLUE_HAVE_DOCOPT:BOOL=FALSE
+    ###     -DLUE_HAVE_FMT:BOOL=FALSE
+    ###     -DLUE_HAVE_NLOHMANN_JSON:BOOL=FALSE
+    ###     -DLUE_HAVE_PYBIND11:BOOL=FALSE
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=3
+    ###     -DLUE_QA_TEST_HPX_RUNWRAPPER=mpi
+    ###     -DLUE_QA_TEST_HPX_PARCELPORT=mpi
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=10
+    ### # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
+    ### LUE_ROUTING_DATA="/scratch/depfg/jong0137/data/routing"
+    ### LUE_BENCHMARK_DATA="/scratch/depfg/jong0137/data/project/lue/benchmark"
 
     conda activate lue
 
 elif [[ $LUE_HOSTNAME == "snellius" ]];
 then
     # Platform for production runs.
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
-        -DLUE_FRAMEWORK_WITH_PYTHON_API:BOOL=TRUE
-        -DLUE_BUILD_VIEW:BOOL=FALSE
-        -DLUE_HAVE_DOCOPT:BOOL=FALSE
-        -DLUE_HAVE_FMT:BOOL=FALSE
-        -DLUE_HAVE_NLOHMANN_JSON:BOOL=TRUE
-        -DLUE_HAVE_PYBIND11:BOOL=TRUE
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=3
-        -DLUE_QA_TEST_HPX_RUNWRAPPER=mpi
-        -DLUE_QA_TEST_HPX_PARCELPORT=mpi
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=32
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_BUILD_DOCUMENTATION:BOOL=FALSE
+    ###     -DLUE_FRAMEWORK_WITH_PYTHON_API:BOOL=TRUE
+    ###     -DLUE_BUILD_VIEW:BOOL=FALSE
+    ###     -DLUE_HAVE_DOCOPT:BOOL=FALSE
+    ###     -DLUE_HAVE_FMT:BOOL=FALSE
+    ###     -DLUE_HAVE_NLOHMANN_JSON:BOOL=TRUE
+    ###     -DLUE_HAVE_PYBIND11:BOOL=TRUE
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=3
+    ###     -DLUE_QA_TEST_HPX_RUNWRAPPER=mpi
+    ###     -DLUE_QA_TEST_HPX_PARCELPORT=mpi
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=32
     # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
     # LUE_ROUTING_DATA="/scratch/depfg/jong0137/data/routing"
     # LUE_BENCHMARK_DATA="/scratch/depfg/jong0137/data/project/lue/benchmark"
@@ -256,14 +248,14 @@ then
 
 elif [[ $LUE_HOSTNAME == "snowdon" ]];
 then
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=2
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=4
-    # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
-    LUE_ROUTING_DATA="$HOME/development/data/project/routing"
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=2
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=2
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=4
+    ### # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
+    ### LUE_ROUTING_DATA="$HOME/development/data/project/routing"
 
     source $LUE/env/bin/activate
 
@@ -271,22 +263,22 @@ elif [[ $LUE_HOSTNAME == "velocity" ]];
 then
     # Platform for development. All 3rd party packages should be available on the system.
     # TODO boost, gdal, hdf5
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_HAVE_DOCOPT:BOOL=FALSE
-        -DLUE_HAVE_BOOST:BOOL=TRUE
-        -DLUE_HAVE_FMT:BOOL=FALSE
-        -DLUE_HAVE_GDAL:BOOL=TRUE
-        -DLUE_HAVE_GLFW:BOOL=TRUE
-        -DLUE_HAVE_HDF5:BOOL=TRUE
-        -DLUE_HAVE_NLOHMANN_JSON:BOOL=TRUE
-        -DLUE_HAVE_PYBIND11:BOOL=TRUE
-        -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=3
-        -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=3
-    "
-    CMAKE_BUILD_PARALLEL_LEVEL=10
-    # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
-    LUE_ROUTING_DATA="/todo/doesnotexist"
+    ### LUE_CMAKE_ARGUMENTS="
+    ###     $LUE_CMAKE_ARGUMENTS
+    ###     -DLUE_HAVE_DOCOPT:BOOL=FALSE
+    ###     -DLUE_HAVE_BOOST:BOOL=TRUE
+    ###     -DLUE_HAVE_FMT:BOOL=FALSE
+    ###     -DLUE_HAVE_GDAL:BOOL=TRUE
+    ###     -DLUE_HAVE_GLFW:BOOL=TRUE
+    ###     -DLUE_HAVE_HDF5:BOOL=TRUE
+    ###     -DLUE_HAVE_NLOHMANN_JSON:BOOL=TRUE
+    ###     -DLUE_HAVE_PYBIND11:BOOL=TRUE
+    ###     -DLUE_QA_TEST_NR_LOCALITIES_PER_TEST=3
+    ###     -DLUE_QA_TEST_NR_THREADS_PER_LOCALITY=3
+    ### "
+    ### CMAKE_BUILD_PARALLEL_LEVEL=10
+    ### # PYTHONPATH=$LUE_OBJECTS/lib:$PYTHONPATH
+    ### LUE_ROUTING_DATA="/todo/doesnotexist"
 
     export CONAN_USER_HOME="$PROJECTS/.."
 
@@ -295,35 +287,36 @@ then
 fi
 
 
-if [[ $MY_DEVENV_BUILD_TYPE == "Debug" ]];
-then
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_ASSERT_CONDITIONS:BOOL=TRUE
-    "
-
-    # LUE_CMAKE_ARGUMENTS="
-    #     $LUE_CMAKE_ARGUMENTS
-    #     -DLUE_ENABLE_CPPCHECK:BOOL=FALSE
-    #     -DLUE_ENABLE_CLANG_TIDY:BOOL=FALSE
-    # "
-
-elif [[ $MY_DEVENV_BUILD_TYPE == "RelWithDebInfo" ]];
-then
-    LUE_CMAKE_ARGUMENTS="
-        $LUE_CMAKE_ARGUMENTS
-        -DLUE_BUILD_OTF2:BOOL=TRUE
-    "
-fi
-
-
-unset repository_cache cmake_toolchain_file
+### if [[ $MY_DEVENV_BUILD_TYPE == "Debug" ]];
+### then
+###     LUE_CMAKE_ARGUMENTS="
+###         $LUE_CMAKE_ARGUMENTS
+###         -DLUE_ASSERT_CONDITIONS:BOOL=TRUE
+###     "
+### 
+###     # LUE_CMAKE_ARGUMENTS="
+###     #     $LUE_CMAKE_ARGUMENTS
+###     #     -DLUE_ENABLE_CPPCHECK:BOOL=FALSE
+###     #     -DLUE_ENABLE_CLANG_TIDY:BOOL=FALSE
+###     # "
+### 
+### elif [[ $MY_DEVENV_BUILD_TYPE == "RelWithDebInfo" ]];
+### then
+###     LUE_CMAKE_ARGUMENTS="
+###         $LUE_CMAKE_ARGUMENTS
+###         -DLUE_BUILD_OTF2:BOOL=TRUE
+###     "
+### fi
 
 
-export LUE_CMAKE_ARGUMENTS
-export CMAKE_BUILD_PARALLEL_LEVEL
-export LD_LIBRARY_PATH
-export LUE_OBJECTS LUE_DATA LUE_HOSTNAME LUE_ROUTING_DATA LUE_BENCHMARK_DATA
+### unset repository_cache cmake_toolchain_file
+
+
+# export LUE_CMAKE_ARGUMENTS
+# export CMAKE_BUILD_PARALLEL_LEVEL
+# export LD_LIBRARY_PATH
+# export LUE_OBJECTS LUE_DATA LUE_HOSTNAME LUE_ROUTING_DATA LUE_BENCHMARK_DATA
+export LUE_OBJECTS
 export PATH
 # export PYTHONPATH
 
---
+++ script/bootstrap_lue.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+set -e
+
+
+if [ -z ${MY_DEVENV+x} ];
+then
+    echo "\$MY_DEVENV is unset"
+    exit 1
+fi
+
+if [ -z ${LUE+x} ];
+then
+    echo "\$LUE is unset"
+    exit 1
+fi
+
+if [ -z ${OBJECTS+x} ];
+then
+    echo "\$OBJECTS is unset"
+    exit 1
+fi
+
+
+usage="$(basename $0) hostname build_type"
+
+
+if [[ $# != 2 ]];
+then
+    echo $usage
+    exit 1
+fi
+
+hostname=$1
+build_type=$2
+
+if [[ $hostname == m1compiler ]]; then
+    compiler="clang"
+    conan_packages="docopt.cpp imgui span-lite"
+else
+    "Unknown hostname: $hostname"
+fi
+
+echo "Setting up $build_type build on $hostname"
+echo "hostname      : $hostname"
+echo "build type    : $build_type"
+echo "conan packages: $conan_packages"
+echo "compiler      : $compiler"
+
+source_dir="$LUE"
+build_dir="$OBJECTS/conan/$build_type/lue"
+
+python "$source_dir/environment/script/write_conan_profile.py" $compiler $source_dir/host_profile
+python "$source_dir/environment/script/write_conan_profile.py" $compiler $source_dir/build_profile
+
+mkdir -p $build_dir
+
+LUE_CONAN_PACKAGES="$conan_packages" \
+    conan install $source_dir \
+        --profile:host=$source_dir/host_profile \
+        --profile:build=$source_dir/build_profile \
+        --settings=build_type=$build_type \
+        --build=missing \
+        --output-folder=$build_dir
+
+ln -s -f $MY_DEVENV/configuration/project/lue/CMakeUserPresets-base.json $source_dir
+ln -s -f $MY_DEVENV/configuration/project/lue/CMakeUserPresets-Conan${build_type}.json $source_dir/CMakeUserPresets.json
+
+ln -s -f $build_dir/CMakePresets.json $source_dir/CMakeConanPresets.json
+cmake -S $source_dir --preset ${hostname}_conan_${build_type,,}
+
+ln -s -f $build_dir/compile_commands.json $source_dir
