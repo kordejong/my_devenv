7d3dbaa5954fa2a51cfe0c6c4bd5e9f2cf643e17
Kor de Jong <kor@jemig.eu>
2024-01-06 11:32:41 +0100

    Update pathways config

--- configuration/project/adaptation_pathways.sh
+++ configuration/project/adaptation_pathways.sh
@@ -2,9 +2,7 @@ cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $cwd/util.sh
 unset cwd
 
-
 parse_commandline $*
-
 
 if [ ! "$ADAPTATION_PATHWAYS" ]; then
     export ADAPTATION_PATHWAYS="$PROJECTS/gauja/geoneric/project/adaptation_pathways"
@@ -16,8 +14,6 @@ if [ ! -d "$ADAPTATION_PATHWAYS" ]; then
 fi
 
 export ADAPTATION_PATHWAYS_OBJECTS="$OBJECTS/adaptation_pathways"
-
-PATH="$ADAPTATION_PATHWAYS/source/script:$PATH"
 
 cd $ADAPTATION_PATHWAYS
 
