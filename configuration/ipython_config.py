dc4990411cc7084e3a9d69d994c5cf7a05869e48
Kor de Jong <kor@jemig.eu>
2024-02-08 16:12:45 +0100

    Update ipython config

--- configuration/ipython_config.py
+++ configuration/ipython_config.py
@@ -1,3 +1,6 @@
 # $HOME/.ipython/profile_default/ipython_config.py
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.editing_mode = "vi"
+
+# c.InteractiveShell.colors = "Linux"
+# c.TerminalInteractiveShell.highlighting_style = "nord"
