416def93d30fd0d96082c13f4b044d096d31ffdc
Kor de Jong <kor@jemig.eu>
2024-06-28 08:56:11 +0200

    Don't select vs version

--- script/vs_git.bat
+++ script/vs_git.bat
@@ -1,3 +1,5 @@
 @echo off
+REM set VSCMD_DEBUG=1
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM -vcvars_ver=14.38
 "C:\Program Files\Git\bin\bash" --login -i
