6b3842dc34e3545ad1e04646f7be34c44594db8f
Kor de Jong <kor@jemig.eu>
2024-10-22 17:57:03 +0200

    Update nvim config

--- configuration/nvim/lua/plugins/lspconfig.lua
+++ configuration/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,5 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/README.md
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 
 return {
     {
@@ -12,11 +12,45 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+            -- https://github.com/python-lsp/python-lsp-server
+            require("lspconfig").pylsp.setup({
+                capabilities = capabilities,
+
+                -- on_attach = custom_attach,
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        -- group = "LspAutoformat",
+                        callback = function()
+                            vim.lsp.buf.format({
+                                bufnr = bufnr,
+                                async = false,
+                            })
+                        end
+                    })
+                end,
+
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            -- black = {
+                            --     enabled = true,
+                            -- },
+                            -- flake8 = {
+                            --     enabled = false,
+                            -- },
+                            pyls_isort = {
+                                enables = true,
+                            },
+                        },
+                    },
+                },
+            })
             require("lspconfig").pyright.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             require("lspconfig").clangd.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
 
             -- vim.lsp.set_log_level("debug")  -- :LspInfo / :LspLog
