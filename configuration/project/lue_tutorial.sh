82e8d53f009cb2bb3be2cac1bad62cdefc2830d8
Kor de Jong <kor@jemig.eu>
2024-11-15 16:42:33 +0100

    Update project config

--- configuration/project/lue_tutorial.sh
+++ configuration/project/lue_tutorial.sh
@@ -2,9 +2,7 @@ cwd=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 source $cwd/util.sh
 unset cwd
 
-
 parse_commandline $*
-
 
 if [ ! "$LUE_TUTORIAL" ]; then
     export LUE_TUTORIAL="$PROJECTS/github/computational_geography/lue_tutorial"
@@ -14,16 +12,6 @@ if [ ! -d "$LUE_TUTORIAL" ]; then
     echo "ERROR: directory $LUE_TUTORIAL does not exist..."
     return 1
 fi
-
-basename=`basename $LUE_TUTORIAL`
-
-LUE_TUTORIAL_OBJECTS="$OBJECTS/$MY_DEVENV_BUILD_TYPE/$basename"
-PATH="$LUE_TUTORIAL/environment/script:$PATH"
-
-unset basename
-
-export LUE_TUTORIAL_OBJECTS
-export PATH
 
 cd $LUE_TUTORIAL
 
