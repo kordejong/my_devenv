b3534290784cf162a00a26000af2134f297dfc70
Kor de Jong <kor@jemig.eu>
2025-04-13 22:10:41 +0200

    WIP configure LazyVim

--- configuration/lazyvim/lua/config/autocmds.lua
+++ configuration/lazyvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- https://www.lazyvim.org/configuration/tips
+
+-- Disable autoformat for lua files
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "lua" },
+--   callback = function()
+--     vim.b.autoformat = false
+--   end,
+-- })
