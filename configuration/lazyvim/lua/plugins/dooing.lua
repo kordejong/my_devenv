81b269de69be90a77da0855d9698aaa83314d8ae
Kor de Jong <kor@jemig.eu>
2025-05-23 08:33:54 +0200

    Override save_path config option

--- configuration/lazyvim/lua/plugins/dooing.lua
+++ configuration/lazyvim/lua/plugins/dooing.lua
@@ -1,5 +1,12 @@
 return {
   {
     "atiladefreitas/dooing",
+    opts = {
+      -- save_path = "$PERSONAL_FILES/Documents/lazyvim/dooing_todos.json",
+      -- save_path = "/home/kor/development/project/gauja/kordejong/personal_files/Documents/lazyvim/dooing_todos.json",
+      -- save_path = vim.env.PERSONAL_FILES .. "Documents/lazyvim/dooing_todos.json",
+      -- save_path = os.getenv("PERSONAL_FILES") .. "Documents/lazyvim/dooing_todos.json",
+      save_path = "/home/kor/development/project/gauja/kordejong/personal_files/Documents/lazyvim/dooing_todos.json",
+    },
   },
 }
