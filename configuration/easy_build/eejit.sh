9cb47a5a8ce3085063f345662201bc7345662940
Kor de Jong <kor@jemig.eu>
2025-08-19 10:40:44 +0200

    Add eejit EB script

---
+++ configuration/easy_build/eejit.sh
@@ -0,0 +1,21 @@
+# export MODULEPATH=/scratch/depfg/pcraster/easybuild/foss2023a/modules/all/
+export MODULEPATH=/depfg/easybuild/foss2024a/modules/all/
+# export MODULEPATH=/depfg/easybuild/foss2025a/modules/all/
+
+module load \
+    Boost/1.85.0-GCC-13.3.0 \
+    CMake/development-GCCcore-13.3.0 \
+    GCC/13.3.0 \
+    GDAL/3.10.0-foss-2024a \
+    Graphviz/12.2.0-GCCcore-13.3.0 \
+    HDF5/1.14.5-gompi-2024a \
+    Ninja/1.12.1-GCCcore-13.3.0 \
+    OpenMPI/5.0.3-GCC-13.3.0 \
+    Python/3.12.3-GCCcore-13.3.0 \
+    ccache/4.10.2-GCCcore-13.3.0 \
+    gperftools/2.16-GCCcore-13.3.0 \
+    hwloc/2.10.0-GCCcore-13.3.0 \
+    mold/2.31.0-GCCcore-13.3.0 \
+    pybind11/2.13.6-GCC-13.3.0
+
+# HPX/1.10
