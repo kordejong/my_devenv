18b88a2ed7b17cb51854c4394cad9a4380c7f43c
Kor de Jong <kor@jemig.eu>
2026-04-09 09:52:40 +0200

    Port to new project

--- configuration/easy_build/jureca.sh
+++ configuration/easy_build/jureca.sh
@@ -4,31 +4,37 @@
 # - Compare with Junxian's modules
 # - Configure use of ccache and mold
 
-source $PROJECT_paj2511/User_EB_files/EB_bash.sh
-
 module load Stages/2025
+
+source $PROJECT_paj2600/User_EB_files/EB_bash.sh
+
 module load GCC OpenMPI
-module load \
-    Boost \
-    Doxygen \
-    GDAL \
-    Graphviz \
-    HDF5 \
-    Ninja \
-    Pandoc \
-    Python \
-    ccache \
-    hwloc \
-    jemalloc \
-    matplotlib \
-    mold \
-    nlohmann_json \
-    tmux
+
+# module load \
+#     Doxygen \
+#     tmux
+
+# Load HDF5 explicitly, and after GDAL so we get the HDF5 library supporting parallel I/O
 module load \
     Clang \
-    CMake/4.0.3
+    CMake/4.0.3 \
+    GDAL \
+    Graphviz \
+    matplotlib \
+    Ninja \
+    Pandoc \
+    ccache \
+    mold \
+    nlohmann_json
 module load \
-    HPX/1.11.0-RelWithDebInfo
+    HDF5
+
+hpx_build_type=RelWithDebInfo
+hpx_build_type=Debug
+module load \
+    HPX/1.11.0-${hpx_build_type}
+
+echo "Loaded HPX with build type ${hpx_build_type}"
 
 # module load HPX/1.10.0-RelDeb
 # module load HPX/1.11.x-RelWithDebInfo
